6683a059c26ab0d2313d8815077d9d1a453b968a
compota334 <compota334@gmail.com>
2024-07-20 20:35:16 +0200

    Update telegram_bot.py

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -41,12 +41,7 @@ class ChatGPTTelegramBot:
         self.commands = [
             
         ]
-        # If imaging is enabled, add the "image" command to the list
-        if self.config.get('enable_image_generation', False):
-            self.commands.append(BotCommand(command='image', description=localized_text('image_description', bot_language)))
-
-        if self.config.get('enable_tts_generation', False):
-            self.commands.append(BotCommand(command='tts', description=localized_text('tts_description', bot_language)))
+               
 
         self.group_commands = [BotCommand(
             command='chat', description=localized_text('chat_description', bot_language)
